9ec26e05dee9626130e2efa73abfd73f2d633d50
Enrico Zini <enrico@enricozini.org>
2016-03-14 12:53:05 +0200

    Tested/perfected sync with taskwarrior

--- egtlib/taskwarrior.py
+++ egtlib/taskwarrior.py
@@ -25,8 +25,14 @@ class Taskwarrior:
         # Taskwarrior lines
         self._lines = []
 
-        # Taskwarrior interface
-        self.tw = taskw.TaskWarrior(marshal=True)
+        # Taskwarrior interface, loaded lazily
+        self._tw = None
+
+    @property
+    def tw(self):
+        if self._tw is None:
+            self._tw = taskw.TaskWarrior(marshal=True)
+        return self._tw
 
     def parse(self, lines):
         """
@@ -42,6 +48,15 @@ class Taskwarrior:
             if not l: break
             self._lines.append(l)
 
+    def _task_to_line(self, task):
+        res = ["t{}".format(task["id"])]
+        res.append("[{:%Y-%m-%d %H:%M} {}]".format(task["modified"], task["status"]))
+        res.append(task["description"])
+        if "tags" in task:
+            bl = self.project.tags
+            res.extend("+" + t for t in task["tags"] if t not in bl)
+        return " ".join(res)
+
     def _sync_line(self, indent, id, body):
         """
         Returns a line build by syncing the information from a parsed next
@@ -50,7 +65,7 @@ class Taskwarrior:
          - If ID is unset, create a new task in taskwarrior using body
          - If ID is set, reload the body from taskwarrior
 
-        Returns None if there is nothing to be changed.
+        Returns None if the line should be deleted.
         """
         if not id:
             desc = []
@@ -64,31 +79,47 @@ class Taskwarrior:
             task = self.tw.task_add(" ".join(desc), project=self.project.name, tags=sorted(tags))
             id = task["id"]
         else:
-            id, task = tw.get_task(id=int(id))
+            new_id, task = self.tw.get_task(id=int(id))
+            if new_id is None:
+                log.warn("taskwarrior #%d does not exist", int(id))
+                return None, None
+            if task["status"] == "deleted":
+                return None, None
+            id = new_id
 
         if task["project"] != self.project.name:
             log.warn("taskwarrior #%d has project %s instead of %s", task["id"], task["project"], self.project.name)
-            return None
-        res = ["t{}".format(task["id"])]
-        res.append("[{:%Y-%m-%d %H:%M} {}]".format(task["modified"], task["status"]))
-        res.append(task["description"])
-        if "tags" in task:
-            bl = self.project.tags
-            res.extend("+" + t for t in task["tags"] if t not in bl)
-        return indent + " ".join(res)
+            return None, None
+        return id, indent + self._task_to_line(task)
 
     def sync_with_taskwarrior(self):
         """
         Sync next actions lines with Taskwarrior.
         """
-        for idx in range(len(self._lines)):
-            line = self._lines[idx]
+        seen = set()
+        new_lines = []
+        for line in self._lines:
             mo = self.re_line.match(line)
             # Skip lines that we do not recognize
-            if not mo: continue
-            new_line = self._sync_line(**mo.groupdict())
-            if new_line is None: continue
-            self._lines[idx] = new_line
+            if not mo:
+                # Lines we do not recognize, we leave as is
+                new_lines.append(line)
+            else:
+                id, new_line = self._sync_line(**mo.groupdict())
+                if id is None:
+                    # Lines with IDs that taskwarrior does not know about, we
+                    # remove
+                    continue
+                else:
+                    new_lines.append(new_line)
+                    seen.add(id)
+
+        for task in self.tw.filter_tasks({"project": "egt"}):
+            if task["id"] in seen: continue
+            if task["status"] == "deleted": continue
+            new_lines.append(self._task_to_line(task))
+
+        self._lines = new_lines
 
     def print(self, out):
         """
