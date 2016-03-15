dc81b2e25bf981b365a586c74406c6168da595ca
Enrico Zini <enrico@enricozini.org>
2016-03-15 20:44:01 +0100

    Unit testing for Log

---
+++ test/test_log.py
@@ -0,0 +1,57 @@
+# coding: utf8
+import unittest
+from .utils import ProjectTestMixin
+from egtlib import Project
+import io
+import os
+import datetime
+
+
+class TestLog(ProjectTestMixin, unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.projectfile = os.path.join(self.workdir.name, ".egt")
+
+    def write_project(self, log_lines):
+        with open(self.projectfile, "wt") as fd:
+            print("Name: testprj", file=fd)
+            print("Tags: testtag1, testtag2", file=fd)
+            print(file=fd)
+            for l in log_lines:
+                print(l, file=fd)
+            print(file=fd)
+            print("hypothetic plans", file=fd)
+
+    def testParse(self):
+        """
+        Test creation of new taskwarrior tasks from a project file
+        """
+        self.write_project([
+            "2015",
+            "15 march: 9:00-12:00",
+            " - tested things",
+        ])
+        proj = Project(self.projectfile, statedir=self.workdir.name)
+        proj.body.force_load_tw(config_filename=self.taskrc)
+        proj.load()
+
+        from egtlib.log import Timebase, Entry
+
+        self.assertEqual(len(proj.log), 2)
+        self.assertIsInstance(proj.log[0], Timebase)
+        self.assertIsInstance(proj.log[1], Entry)
+        self.assertEquals(proj.log[0].dt, datetime.datetime(2015, 1, 1))
+        entry = proj.log[1]
+        self.assertEqual(entry.begin, datetime.datetime(2015, 3, 15, 9))
+        self.assertEqual(entry.until, datetime.datetime(2015, 3, 15, 12))
+        self.assertEqual(entry.head, "15 march: 9:00-12:00")
+        self.assertEqual(entry.body, [" - tested things"])
+
+        with io.StringIO() as out:
+            proj.log.print(out)
+            body_lines = out.getvalue().splitlines()
+
+        self.assertEqual(len(body_lines), 3)
+        self.assertEqual(body_lines[0], "2015")
+        self.assertEqual(body_lines[1], "15 March: 09:00-12:00 3h")
+        self.assertEqual(body_lines[2], " - tested things")
