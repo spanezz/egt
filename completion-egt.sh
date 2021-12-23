3157e24db84c5f561ec0d6bcba63b8a361073056
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2021-12-23 07:27:43 +0100

    fix whitespace

--- completion-egt.sh
+++ completion-egt.sh
@@ -1,27 +1,27 @@
 _egt() {
-	COMPREPLY=()
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-	local prev="${COMP_WORDS[COMP_CWORD-1]}"
+    COMPREPLY=()
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-	case "$prev" in
+    case "$prev" in
         egt)
-			local commands=$(egt completion commands)
-			COMPREPLY=($(compgen -W "$commands" -- "$cur"))
+            local commands=$(egt completion commands)
+            COMPREPLY=($(compgen -W "$commands" -- "$cur"))
             return 0
         ;;
-		-t|--tag)
-			local tags=$(egt completion tags)
-			COMPREPLY=($(compgen -W "$tags" -- "$cur"))
-		;;
+        -t|--tag)
+            local tags=$(egt completion tags)
+            COMPREPLY=($(compgen -W "$tags" -- "$cur"))
+        ;;
     esac
 
-	local base="${COMP_WORDS[1]}"
-	case "$base" in
-		edit|grep|term|work|print_log)
-			local projects=$(egt completion projects)
-			COMPREPLY=($(compgen -W "$projects" -- "$cur"))
-		;;
-	esac
-	return 0
+    local base="${COMP_WORDS[1]}"
+    case "$base" in
+        edit|grep|term|work|print_log)
+            local projects=$(egt completion projects)
+            COMPREPLY=($(compgen -W "$projects" -- "$cur"))
+        ;;
+    esac
+    return 0
 } 
 complete -F _egt egt
