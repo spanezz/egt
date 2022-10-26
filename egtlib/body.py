368a6ae9d27e7c7a9f9ddc9bd8d6bf939edde5fe
Enrico Zini <enrico@enricozini.org>
2022-10-26 14:03:03 +0200

    Removed unused import

--- egtlib/body.py
+++ egtlib/body.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import re
 import shlex
-from typing import Any, Dict, List, Optional, TextIO, Union
+from typing import Dict, List, Optional, TextIO, Union
 
 import taskw
 
